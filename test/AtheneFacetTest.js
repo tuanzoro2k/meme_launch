4d25c9560c1c2f9ebe086bf42447d5a3223ee86b
tuanzoro2k <nguyentientuan20052000@gmail.com>
2024-11-20 17:52:49 +0700

    add test for facets

---
+++ test/AtheneFacetTest.js
@@ -0,0 +1,375 @@
+
+const { expect } = require("chai");
+const { ethers } = require("hardhat")
+const { getSelectors, FacetCutAction } = require('../scripts/libraries/diamond.js')
+const helpers = require("@nomicfoundation/hardhat-network-helpers");
+
+
+describe("Athene Facet", () => {
+    let atheneDiamond;
+    let owner;
+    let operator1;
+    let operator2;
+    let router1;
+    let router2;
+    let configIndex, startTime, buyFeeRate, sellFeeRate, maxBuyAmount, delayBuyTime, initialBuyAmount;
+    const FEE_DENOMINATOR = 1e4;
+    let atheneFacet
+    let managerFacet
+    before(async function () {
+        [owner, operator1, operator2, router1, router2] = await ethers.getSigners();
+        const DiamondInit = await ethers.getContractFactory('DiamondInit')
+        const diamondInit = await DiamondInit.deploy()
+        await diamondInit.deployed()
+
+        // Deploy facets and set the `facetCuts` variable
+        const FacetNames = [
+            'AtheneFacet',
+            'ManagerFacet',
+        ]
+        // The `facetCuts` variable is the FacetCut[] that contains the functions to add during diamond deployment
+        const facetCuts = []
+        for (const FacetName of FacetNames) {
+            const Facet = await ethers.getContractFactory(FacetName)
+            const facet = await Facet.deploy()
+            await facet.deployed()
+            console.log(`${FacetName} deployed: ${facet.address}`)
+            facetCuts.push({
+                facetAddress: facet.address,
+                action: FacetCutAction.Add,
+                functionSelectors: getSelectors(facet)
+            })
+        }
+
+        // Creating a function call
+        // This call gets executed during deployment and can also be executed in upgrades
+        // It is executed with delegatecall on the DiamondInit address.
+        let functionCall = diamondInit.interface.encodeFunctionData('init')
+
+        // Setting arguments that will be used in the diamond constructor
+        const diamondArgs = {
+            owner: owner.address,
+            init: diamondInit.address,
+            initCalldata: functionCall
+        }
+
+        // Deploy AtheneDiamond contract
+        const AtheneDiamond = await ethers.getContractFactory("AtheneDiamond");
+        atheneDiamond = await AtheneDiamond.deploy(
+            facetCuts,
+            diamondArgs
+        );
+        await atheneDiamond.deployed()
+
+        // Set up initial values
+        configIndex = 0;
+        startTime = Math.floor(Date.now() / 1000) + 3600; // 1 hour from now
+        buyFeeRate = 100; // 1%
+        sellFeeRate = 200; // 2%
+        maxBuyAmount = ethers.utils.parseEther("1000");
+        delayBuyTime = 3600; // 1 hour
+        initialBuyAmount = ethers.utils.parseEther("100");
+
+        atheneFacet = await ethers.getContractAt('AtheneFacet', atheneDiamond.address)
+        managerFacet = await ethers.getContractAt('ManagerFacet', atheneDiamond.address)
+        const wethAddress = "0xa084b43e09c74E401b7500c3D30a1B569e4934AD"
+        const feeReceiver = "0xa084b43e09c74E401b7500c3D30a1B569e4934AD"
+        const feeBps = 50
+        const refBps = 50
+        const tx = await managerFacet.setMasterConfig(wethAddress, feeReceiver, feeBps, refBps)
+        await tx.wait()
+
+        await managerFacet.setPoolConfig(configIndex, {
+            index: configIndex,
+            initialVirtualBaseReserve: ethers.utils.parseEther("1000"),
+            initialVirtualQuoteReserve: ethers.utils.parseEther("1000"),
+            totalSellingBaseAmount: ethers.utils.parseEther("500"),
+            maxListingBaseAmount: ethers.utils.parseEther("500"),
+            maxListingQuoteAmount: ethers.utils.parseEther("500"),
+            defaultListingRate: 10000,
+            listingFee: ethers.utils.parseEther("10"),
+        });
+
+        await managerFacet.setWhitelistedRouters([router1.address, router2.address], true);
+    });
+
+    it("should create a new pool successfully without initialBuy", async function () {
+
+        const tx = await atheneFacet.createPool({
+            name: "Test Token",
+            symbol: "TEST",
+            poolDetails: "This is a test pool",
+            configIndex: configIndex,
+            router: router1.address,
+            startTime: startTime,
+            buyFeeRate: buyFeeRate,
+            sellFeeRate: sellFeeRate,
+            maxBuyAmount: maxBuyAmount,
+            delayBuyTime: delayBuyTime,
+            merkleRoot: "0x0000000000000000000000000000000000000000000000000000000000000000",
+            initialBuyAmount: 0,
+        });
+
+        const receipt = await tx.wait();
+        // Extract the TokenCreated event from the logs
+        const tokenCreatedEvent = receipt.events.find(event => event.event === "TokenCreated");
+        expect(tokenCreatedEvent).to.not.be.undefined;
+
+        const tokenAddress = tokenCreatedEvent.args.token;
+        // Check if the token was created
+        const token = await ethers.getContractAt('AtheneToken', tokenAddress);
+        expect(await token.name()).to.equal("Test Token");
+        expect(await token.symbol()).to.equal("TEST");
+
+        // Check pool info
+        const poolInfo = await managerFacet.getPoolInfo(tokenAddress);
+        expect(poolInfo.owner).to.equal(owner.address);
+        expect(poolInfo.token).to.equal(tokenAddress);
+        expect(poolInfo.router).to.equal(router1.address);
+        expect(poolInfo.poolDetails).to.equal("This is a test pool");
+        expect(poolInfo.state).to.equal(0);
+        expect(poolInfo.startTime).to.equal(startTime);
+        expect(poolInfo.buyFeeRate).to.equal(buyFeeRate);
+        expect(poolInfo.sellFeeRate).to.equal(sellFeeRate);
+        expect(poolInfo.maxBuyAmount).to.equal(maxBuyAmount);
+        expect(poolInfo.delayBuyTime).to.equal(delayBuyTime);
+        expect(poolInfo.whitelistMerkleRoot).to.equal('0x0000000000000000000000000000000000000000000000000000000000000000');
+    });
+
+    it("should create a new pool successfully with initialBuy", async function () {
+        const initialBuy = ethers.utils.parseEther('0.1');
+
+        const masterConfig = await managerFacet.getMasterConfig()
+        const totalFee = (initialBuy * masterConfig.feeBps) / FEE_DENOMINATOR + (initialBuy * buyFeeRate) / FEE_DENOMINATOR;
+        const tx = await atheneFacet.createPool({
+            name: "Test Token",
+            symbol: "TEST",
+            poolDetails: "This is a test pool",
+            configIndex: configIndex,
+            router: router1.address,
+            startTime: startTime,
+            buyFeeRate: buyFeeRate,
+            sellFeeRate: sellFeeRate,
+            maxBuyAmount: maxBuyAmount,
+            delayBuyTime: delayBuyTime,
+            merkleRoot: "0x0000000000000000000000000000000000000000000000000000000000000000",
+            initialBuyAmount: initialBuy,
+        }, { value: initialBuy.add(totalFee) });
+
+        const receipt = await tx.wait();
+        // Extract the TokenCreated event from the logs
+        const tokenCreatedEvent = receipt.events.find(event => event.event === "TokenCreated");
+        expect(tokenCreatedEvent).to.not.be.undefined;
+
+        const tokenAddress = tokenCreatedEvent.args.token;
+
+        // Check if the token was created
+        const token = await ethers.getContractAt('AtheneToken', tokenAddress);
+        expect(await token.name()).to.equal("Test Token");
+        expect(await token.symbol()).to.equal("TEST");
+
+        // Check pool info
+
+        const poolInfo = await managerFacet.getPoolInfo(tokenAddress);
+        expect(poolInfo.owner).to.equal(owner.address);
+        expect(poolInfo.token).to.equal(tokenAddress);
+        expect(poolInfo.router).to.equal(router1.address);
+        expect(poolInfo.poolDetails).to.equal("This is a test pool");
+        expect(poolInfo.state).to.equal(0);
+        expect(poolInfo.startTime).to.equal(startTime);
+        expect(poolInfo.buyFeeRate).to.equal(buyFeeRate);
+        expect(poolInfo.sellFeeRate).to.equal(sellFeeRate);
+        expect(poolInfo.maxBuyAmount).to.equal(maxBuyAmount);
+        expect(poolInfo.delayBuyTime).to.equal(delayBuyTime);
+        expect(poolInfo.whitelistMerkleRoot).to.equal('0x0000000000000000000000000000000000000000000000000000000000000000');
+
+        expect(Number(await token.balanceOf(owner.address))).to.be.greaterThan(0)
+    });
+
+    it("should revert if the router is invalid", async function () {
+        await expect(
+            atheneFacet.createPool({
+                name: "Test Token",
+                symbol: "TEST",
+                poolDetails: "This is a test pool",
+                configIndex: configIndex,
+                router: operator1.address, // Invalid router
+                startTime: startTime,
+                buyFeeRate: buyFeeRate,
+                sellFeeRate: sellFeeRate,
+                maxBuyAmount: maxBuyAmount,
+                delayBuyTime: delayBuyTime,
+                merkleRoot: "0x0000000000000000000000000000000000000000000000000000000000000000",
+                initialBuyAmount: 0,
+            })
+        ).to.be.revertedWith("AtheneFacet: Invalid router");
+    });
+
+    it("should revert if the config index is invalid", async function () {
+        await expect(
+            atheneFacet.createPool({
+                name: "Test Token",
+                symbol: "TEST",
+                poolDetails: "This is a test pool",
+                configIndex: 1, // Invalid config index
+                router: router1.address,
+                startTime: startTime,
+                buyFeeRate: buyFeeRate,
+                sellFeeRate: sellFeeRate,
+                maxBuyAmount: maxBuyAmount,
+                delayBuyTime: delayBuyTime,
+                merkleRoot: "0x0000000000000000000000000000000000000000000000000000000000000000",
+                initialBuyAmount: 0,
+            })
+        ).to.be.revertedWith("AtheneFacet: Invalid config");
+    });
+
+    it("should revert if the fee rates are invalid", async function () {
+        await expect(
+            atheneFacet.createPool({
+                name: "Test Token",
+                symbol: "TEST",
+                poolDetails: "This is a test pool",
+                configIndex: configIndex,
+                router: router1.address,
+                startTime: startTime,
+                buyFeeRate: FEE_DENOMINATOR + 1, // Invalid fee rate
+                sellFeeRate: sellFeeRate,
+                maxBuyAmount: maxBuyAmount,
+                delayBuyTime: delayBuyTime,
+                merkleRoot: "0x0000000000000000000000000000000000000000000000000000000000000000",
+                initialBuyAmount: 0,
+            })
+        ).to.be.revertedWith("AtheneFacet: Invalid fee rate");
+    });
+
+    it("should revert if the initial buy amount is wrong", async function () {
+        await expect(
+            atheneFacet.createPool({
+                name: "Test Token",
+                symbol: "TEST",
+                poolDetails: "This is a test pool",
+                configIndex: configIndex,
+                router: router1.address,
+                startTime: startTime,
+                buyFeeRate: buyFeeRate,
+                sellFeeRate: sellFeeRate,
+                maxBuyAmount: maxBuyAmount,
+                delayBuyTime: delayBuyTime,
+                merkleRoot: "0x0000000000000000000000000000000000000000000000000000000000000000",
+                initialBuyAmount: ethers.utils.parseEther("1000000"),
+            })
+        ).to.be.revertedWith("AtheneFacet: Invalid input amount");
+    });
+
+    it("should revert if pool has not started", async function () {
+        const tx = await atheneFacet.createPool({
+            name: "Test Buy Token",
+            symbol: "TEST",
+            poolDetails: "This is a test pool",
+            configIndex: configIndex,
+            router: router1.address,
+            startTime: startTime,
+            buyFeeRate: buyFeeRate,
+            sellFeeRate: sellFeeRate,
+            maxBuyAmount: maxBuyAmount,
+            delayBuyTime: delayBuyTime,
+            merkleRoot: "0x0000000000000000000000000000000000000000000000000000000000000000",
+            initialBuyAmount: 0,
+        });
+        const receipt = await tx.wait();
+        // Extract the TokenCreated event from the logs
+        const tokenCreatedEvent = receipt.events.find(event => event.event === "TokenCreated");
+        expect(tokenCreatedEvent).to.not.be.undefined;
+
+        const tokenAddress = tokenCreatedEvent.args.token;
+
+        const buyer = operator1;
+        const amountIn = ethers.utils.parseEther("0.1");
+        const { amountOut } = await atheneFacet.getAmountOut(tokenAddress, amountIn, true);
+        const minAmountOut = amountOut.mul(95).div(100);
+
+        // Act & Assert
+        await expect(
+            atheneFacet.connect(buyer).buy(tokenAddress, owner.address, amountIn, minAmountOut, [])
+        ).to.be.revertedWith("AtheneFacet: Not started");
+    });
+
+    it("should allow buying when all conditions are met", async function () {
+        const tx = await atheneFacet.createPool({
+            name: "Test Buy Token",
+            symbol: "TEST",
+            poolDetails: "This is a test pool",
+            configIndex: configIndex,
+            router: router1.address,
+            startTime: startTime,
+            buyFeeRate: buyFeeRate,
+            sellFeeRate: sellFeeRate,
+            maxBuyAmount: maxBuyAmount,
+            delayBuyTime: delayBuyTime,
+            merkleRoot: "0x0000000000000000000000000000000000000000000000000000000000000000",
+            initialBuyAmount: 0,
+        });
+        const receipt = await tx.wait();
+        // Extract the TokenCreated event from the logs
+        const tokenCreatedEvent = receipt.events.find(event => event.event === "TokenCreated");
+        expect(tokenCreatedEvent).to.not.be.undefined;
+
+        const tokenAddress = tokenCreatedEvent.args.token;
+
+        const buyer = operator1;
+        const amountIn = ethers.utils.parseEther("0.1");
+        const { amountOut, totalFee } = await atheneFacet.getAmountOut(tokenAddress, amountIn, true);
+        const minAmountOut = amountOut.mul(95).div(100);
+
+
+        await helpers.time.increase(3600)
+
+        const tx2 = await atheneFacet.connect(buyer).buy(tokenAddress, owner.address, amountIn, minAmountOut, [], { value: amountIn.add(totalFee) })
+        const poolInfo = await managerFacet.getPoolInfo(tokenAddress);
+        const receipt2 = await tx2.wait();
+
+        const buyToken = receipt2.events.find(event => event.event === "Trade");
+
+        expect(buyToken.args.token).to.equal(tokenAddress)
+        expect(buyToken.args.user).to.equal(buyer.address)
+        expect(buyToken.args.amountIn).to.equal(amountIn)
+        expect(buyToken.args.isBuy).to.equal(true)
+        expect(buyToken.args.baseReserve).to.equal(poolInfo.virtualBaseReserve)
+        expect(buyToken.args.quoteReserve).to.equal(poolInfo.virtualQuoteReserve)
+    });
+
+    it("should revert if max buy amount exceeded", async function () {
+        const tx = await atheneFacet.createPool({
+            name: "Test Buy Token",
+            symbol: "TEST",
+            poolDetails: "This is a test pool",
+            configIndex: configIndex,
+            router: router1.address,
+            startTime: startTime,
+            buyFeeRate: buyFeeRate,
+            sellFeeRate: sellFeeRate,
+            maxBuyAmount: maxBuyAmount,
+            delayBuyTime: delayBuyTime,
+            merkleRoot: "0x0000000000000000000000000000000000000000000000000000000000000000",
+            initialBuyAmount: 0,
+        });
+        const receipt = await tx.wait();
+        // Extract the TokenCreated event from the logs
+        const tokenCreatedEvent = receipt.events.find(event => event.event === "TokenCreated");
+        expect(tokenCreatedEvent).to.not.be.undefined;
+
+        const tokenAddress = tokenCreatedEvent.args.token;
+        const buyer = operator1;
+        const amountIn = ethers.utils.parseEther("2000"); // Exceeds maxBuyAmount
+        const { amountOut, totalFee } = await atheneFacet.getAmountOut(tokenAddress, amountIn, true);
+        const minAmountOut = amountOut.mul(95).div(100);
+
+        await expect(
+            atheneFacet.connect(buyer).buy(tokenAddress, owner.address, amountIn, minAmountOut, [])
+        ).to.be.revertedWith("AtheneFacet: Exceeded max buy");
+    });
+
+});
+
+
--- test/ManagerFacetTest.js
+++ test/ManagerFacetTest.js
@@ -4,14 +4,20 @@ const { ethers } = require("hardhat")
 const { getSelectors, FacetCutAction } = require('../scripts/libraries/diamond.js')
 
 
-describe("ManagerFacet", function () {
-  it("Should allow setting admin", async function () {
-    const [owner, newAdmin] = await ethers.getSigners();
-
+describe("ManagerFacet", () => {
+  let atheneDiamond;
+  let owner;
+  let operator1;
+  let operator2;
+  let router1;
+  let router2;
+  before(async function () {
+    [owner, operator1, operator2, router1, router2] = await ethers.getSigners();
     const DiamondInit = await ethers.getContractFactory('DiamondInit')
     const diamondInit = await DiamondInit.deploy()
     await diamondInit.deployed()
     console.log('DiamondInit deployed:', diamondInit.address)
+
     // Deploy facets and set the `facetCuts` variable
     console.log('')
     console.log('Deploying facets')
@@ -47,20 +53,154 @@ describe("ManagerFacet", function () {
 
     // Deploy AtheneDiamond contract
     const AtheneDiamond = await ethers.getContractFactory("AtheneDiamond");
-    const atheneDiamond = await AtheneDiamond.deploy(
+    atheneDiamond = await AtheneDiamond.deploy(
       facetCuts,
       diamondArgs
     );
     await atheneDiamond.deployed()
+  });
+
+  it("Should allow setting masterconfig", async () => {
 
     const managerFacet = await ethers.getContractAt('ManagerFacet', atheneDiamond.address)
-    const tx = await managerFacet.setMasterConfig("0xa084b43e09c74E401b7500c3D30a1B569e4934AD", "0xa084b43e09c74E401b7500c3D30a1B569e4934AD", 50, 60)
+    const wethAddress = "0xa084b43e09c74E401b7500c3D30a1B569e4934AD"
+    const feeReceiver = "0xa084b43e09c74E401b7500c3D30a1B569e4934AD"
+    const feeBps = 50
+    const refBps = 50
+    const tx = await managerFacet.setMasterConfig(wethAddress, feeReceiver, feeBps, refBps)
     await tx.wait();
 
-    // Check if new admin is set correctly
+    // Check if new config is set correctly
     const currentConfig = await managerFacet.getMasterConfig();
-    console.log(currentConfig)
+    expect(currentConfig.wethAddress).to.be.equal(wethAddress)
+    expect(currentConfig.feeReceiver).to.be.equal(wethAddress)
+    expect(currentConfig.feeBps).to.be.equal(feeBps)
+    expect(currentConfig.refBps).to.be.equal(refBps)
   });
+
+
+  it("show allow setting paused", async () => {
+    const managerFacet = await ethers.getContractAt('ManagerFacet', atheneDiamond.address)
+    const tx = await managerFacet.setPaused(true)
+    await tx.wait();
+
+    expect(await managerFacet.isPaused()).to.be.equal(true)
+  })
+
+  //Pool config
+  it("show allow setting pool config", async () => {
+    const managerFacet = await ethers.getContractAt('ManagerFacet', atheneDiamond.address)
+
+    const index = 1
+    const athenePoolConfig = {
+      index: 1,
+      initialVirtualBaseReserve: 1000,
+      initialVirtualQuoteReserve: 2000,
+      totalSellingBaseAmount: 1000,
+      maxListingBaseAmount: 10000,
+      maxListingQuoteAmount: 10000,
+      defaultListingRate: 50,
+      listingFee: 10
+    }
+
+    const tx = await managerFacet.setPoolConfig(index, athenePoolConfig);
+    await tx.wait()
+
+    const currentPoolConfig = await managerFacet.getPoolConfig(1);
+    expect(currentPoolConfig.index).to.be.equal(1)
+    expect(currentPoolConfig.initialVirtualBaseReserve).to.be.equal(1000)
+    expect(currentPoolConfig.initialVirtualQuoteReserve).to.be.equal(2000)
+    expect(currentPoolConfig.totalSellingBaseAmount).to.be.equal(1000)
+    expect(currentPoolConfig.maxListingBaseAmount).to.be.equal(10000)
+    expect(currentPoolConfig.maxListingQuoteAmount).to.be.equal(10000)
+    expect(currentPoolConfig.defaultListingRate).to.be.equal(50)
+    expect(currentPoolConfig.listingFee).to.be.equal(10)
+  })
+
+  it("should remove setting pool config", async () => {
+    const managerFacet = await ethers.getContractAt('ManagerFacet', atheneDiamond.address)
+    const tx = await managerFacet.removePoolConfig(1);
+    await tx.wait()
+
+    await expect(managerFacet.getPoolConfig(1)).to.be.revertedWith("ManagerFacet: Invalid config");
+  })
+
+  //Operators
+  it("should allow setting operators", async () => {
+    const managerFacet = await ethers.getContractAt('ManagerFacet', atheneDiamond.address);
+
+    const tx = await managerFacet.setOperators([operator1.address, operator2.address], true);
+    await tx.wait();
+
+    const operators = await managerFacet.getOperators();
+    console.log(operators)
+    expect(operators.includes(operator1.address)).to.be.true;
+    expect(operators.includes(operator2.address)).to.be.true;
+  });
+
+  it("should allow removing operators", async () => {
+    const managerFacet = await ethers.getContractAt('ManagerFacet', atheneDiamond.address);
+
+    const txRemove = await managerFacet.setOperators([operator2.address], false);
+    await txRemove.wait();
+
+    const operators = await managerFacet.getOperators();
+    expect(operators.includes(operator2.address)).to.be.false;
+  });
+
+  //Routers
+  it("should allow setting whitelisted routers", async () => {
+    const managerFacet = await ethers.getContractAt('ManagerFacet', atheneDiamond.address);
+
+    const tx = await managerFacet.setWhitelistedRouters([router1.address, router2.address], true);
+    await tx.wait();
+
+    const routers = await managerFacet.getRouters();
+    expect(routers.includes(router1.address)).to.be.true;
+    expect(routers.includes(router2.address)).to.be.true;
+  });
+
+  it("should allow removing whitelisted routers", async () => {
+    const managerFacet = await ethers.getContractAt('ManagerFacet', atheneDiamond.address);
+
+    const txRemove = await managerFacet.setWhitelistedRouters([router2.address], false);
+    await txRemove.wait();
+
+    const routers = await managerFacet.getRouters();
+    expect(routers.includes(router2.address)).to.be.false;
+  });
+
+  //Pool state
+  it("should allow setting pool state", async () => {
+    const managerFacet = await ethers.getContractAt('ManagerFacet', atheneDiamond.address);
+
+    const token = "0x1234567890123456789012345678901234567890";
+
+    const tx = await managerFacet.setPoolState(token, 1); // Assuming 1 represents an active state
+    await tx.wait();
+
+    const poolInfo = await managerFacet.getPoolInfo(token);
+    expect(poolInfo.state).to.equal(1);
+  });
+
+  it("should not allow changing pool state if not admin or operator", async () => {
+    const managerFacet = await ethers.getContractAt('ManagerFacet', atheneDiamond.address);
+
+    const token = "0x09876543210987654321098765432109876543210";
+
+    // Non-admin/non-operator tries to change state
+    await expect(managerFacet.setPoolState(token, 1))
+      .to.be.revertedWith("ManagerFacet: Not admin or operator");
+  });
+
+  it("show allow setting admin", async () => {
+    const managerFacet = await ethers.getContractAt('ManagerFacet', atheneDiamond.address)
+    const tx = await managerFacet.setAdmin("0xa084b43e09c74E401b7500c3D30a1B569e4934AD")
+    await tx.wait();
+
+    const currentConfig = await managerFacet.getMasterConfig();
+    expect(currentConfig.admin).to.be.equal("0xa084b43e09c74E401b7500c3D30a1B569e4934AD")
+  })
 });
 
 
